3b2a7ebc9b4e89721d15315ebd41de89cd9c9126
Sele3 <STAN266@e.ntu.edu.sg>
2023-06-12 00:25:13 +0800

    feat: implement methods
    - add Message struct
    - implement send and get message methods for redisclient
    - implement send and pull methods for imserviceimpl

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -2,30 +2,119 @@ package main
 
 import (
 	"context"
-	"math/rand"
-
+	"fmt"
+	"strings"
+	"time"
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 )
 
 // IMServiceImpl implements the last service interface defined in the IDL.
 type IMServiceImpl struct{}
 
+func getRoomID(chat string) (string, error) {
+    var roomID string
+    
+    lowercase := strings.ToLower(chat)
+    senders := strings.Split(lowercase, ":")
+    if len(senders) != 2 {
+       err := fmt.Errorf("invalid Chat ID '%s', should be in the format of user1:user2", chat)
+       return "", err
+    }
+    
+    sender1, sender2 := senders[0], senders[1]
+    if comp := strings.Compare(sender1, sender2); comp == 1 {
+       roomID = fmt.Sprintf("%s:%s", sender2, sender1)
+    } else {
+       roomID = fmt.Sprintf("%s:%s", sender1, sender2)
+    }
+
+    return roomID, nil
+}
+
+func validate(req *rpc.SendRequest) error {
+    senders := strings.Split(req.Message.Chat, ":")
+    if len(senders) != 2 {
+       err := fmt.Errorf("invalid Chat ID '%s', should be in the format of user1:user2", req.Message.GetChat())
+       return err
+    }
+    sender1, sender2 := senders[0], senders[1]
+
+    if req.Message.GetSender() != sender1 && req.Message.GetSender() != sender2 {
+       err := fmt.Errorf("sender '%s' not in the chat room", req.Message.GetSender())
+       return err
+    }
+
+    return nil
+}
+
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
-	resp := rpc.NewSendResponse()
-	resp.Code, resp.Msg = areYouLucky()
-	return resp, nil
+	if err := validate(req); err != nil {
+		return nil, err
+	}
+ 
+	timestamp := time.Now().Unix()
+	message := &Message{
+		Message:   req.Message.GetText(),
+		Sender:    req.Message.GetSender(),
+		Timestamp: timestamp,
+	}
+ 
+	roomID, err := getRoomID(req.Message.GetChat())
+	if err != nil {
+		return nil, err 
+	}
+	
+	err = redisClient.SaveMessage(ctx, roomID, message)
+	if err != nil {
+		return nil, err
+	}
+ 
+	return &rpc.SendResponse{
+		Code: 0,
+		Msg:  "success",
+	}, nil	
 }
 
 func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
-	resp := rpc.NewPullResponse()
-	resp.Code, resp.Msg = areYouLucky()
-	return resp, nil
-}
-
-func areYouLucky() (int32, string) {
-	if rand.Int31n(2) == 1 {
-		return 0, "success"
-	} else {
-		return 500, "oops"
+	roomID, err := getRoomID(req.GetChat())
+    if err != nil {
+		return nil, err
 	}
+
+	limit := int64(req.GetLimit())
+    start := req.GetCursor()
+    end := start + limit
+
+    messages, err := redisClient.GetMessages(ctx, roomID, start, end, req.GetReverse())
+    if err != nil {
+		return nil, err
+    }
+
+    respMessages := make([]*rpc.Message, 0)
+    var counter int64 = 0
+    var nextCursor int64 = 0
+    hasMore := false
+    for _, msg := range messages {
+		if counter+1 > limit {
+			hasMore = true
+			nextCursor = end
+			break
+		}
+        temp := &rpc.Message{
+			Chat:     req.GetChat(),
+			Text:     msg.Message,
+			Sender:   msg.Sender,
+			SendTime: msg.Timestamp,
+		}
+		respMessages = append(respMessages, temp)
+		counter += 1
+	}
+
+    return &rpc.PullResponse{
+		Messages:    respMessages,
+		Code:        0,
+		Msg:         "success",
+		HasMore:     &hasMore,
+		NextCursor:  &nextCursor,
+	}, nil	
 }
--- rpc-server/redis.go
+++ rpc-server/redis.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -22,4 +23,53 @@ func (rc *RedisClient) Init(ctx context.Context, address, password string) error
 
 	rc.client = r
 	return nil
+}
+
+func (rc *RedisClient) SaveMessage(ctx context.Context, roomID string, message *Message) error {
+	text, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	member := &redis.Z{
+		Score:  float64(message.Timestamp),
+		Member: text,
+	}
+
+	_, err = rc.client.ZAdd(ctx, roomID, *member).Result()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (rc *RedisClient) GetMessages(ctx context.Context, roomID string, start, end int64, reverse bool) ([]*Message, error) {
+	var (
+		rawMessages []string
+		messages    []*Message
+		err         error
+    )
+
+    if reverse {
+		rawMessages, err = rc.client.ZRevRange(ctx, roomID, start, end).Result()
+    } else {
+		rawMessages, err = rc.client.ZRange(ctx, roomID, start, end).Result()
+    }
+
+	if err != nil {
+		return nil, err
+	}
+	
+
+    for _, msg := range rawMessages {
+       temp := &Message{}
+       err := json.Unmarshal([]byte(msg), temp)
+       if err != nil {
+          return nil, err
+       }
+       messages = append(messages, temp)
+    }
+
+    return messages, nil
 }
